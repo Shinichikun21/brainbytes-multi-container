8f2927631fa6a211b95de74147f8fc208393cde7
Shinichikun21 <124062950+Shinichikun21@users.noreply.github.com>
2025-07-23 17:56:29 +0800

    Delete backend directory

--- backend/models/Sessions.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const SessionSchema = new mongoose.Schema({
-  sessionId: { type: String, required: true, unique: true },
-  createdAt: { type: Date, default: Date.now }
-});
-
-
-module.exports = mongoose.model('Sessions', SessionSchema);
